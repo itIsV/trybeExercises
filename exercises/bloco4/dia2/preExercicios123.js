cc808b646add88002223aa763b32fd4eeb2cc47f
Vinicios Fraga <soicinivt@gmail.com>
2021-02-25 16:28:21 -0300

    Exercicio 1 com for concluido.

--- exercises/bloco4/dia2/preExercicios123.js
+++ exercises/bloco4/dia2/preExercicios123.js
@@ -17,3 +17,11 @@ menu.push('Contato');
 
 console.log (menu);
 
+//Utilize o for para imprimir os elementos da lista groceryList com o console.log()
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let indice = 0; indice < groceryList.length; indice += 1) {
+  console.log (groceryList[indice]);
+};
+
