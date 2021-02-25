83cfda6a2ae25b455b0c19fbbb65bef9978255f0
Vinicios Fraga <soicinivt@gmail.com>
2021-02-25 18:56:21 -0300

    Primeiro requisito completo.

---
+++ exercises/bloco4/dia2/exerciciosArrayEFor.js
@@ -0,0 +1,7 @@
+//requisito 1:
+
+let numbers = [5,9,3,19,70,8,100,2,35,27];
+
+for (let numerosPercorridos = 0; numerosPercorridos < numbers.length; numerosPercorridos +=1) {
+  console.log (numbers[numerosPercorridos]);
+};
