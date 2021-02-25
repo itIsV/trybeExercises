dc5dc366055db232621972e4e3884cbfb093de36
Vinicios Fraga <soicinivt@gmail.com>
2021-02-25 12:34:11 -0300

    Calculadora de subtração pronta.

--- exercises/bloco4/dia1/calculadoraDeModulo.js
+++ exercises/bloco4/dia1/calculadoraDeModulo.js
@@ -11,3 +11,16 @@ switch (true) {
       console.log(((b)**2)**(1/2));
 };
 
+a = true, b = 2;
+
+switch (true) {
+  
+  case (a ** 1 !== a || b ** 1 !== b) :
+      console.log ('Os operadores precisam ser numeros válidos!');
+      break;
+      
+  default:
+      console.log(((a)**2)**(1/2));
+      console.log(((b)**2)**(1/2));
+};
+
---
+++ exercises/bloco4/dia1/calculadoraDeSubtração.js
@@ -0,0 +1,17 @@
+let a = 'z', b = 2;
+
+if (a ** 1 == a && b ** 1 == b) {
+  console.log (a - b)
+}
+else {
+  console.log (' a e b precisam ser numeros válidos')
+};
+
+a = -2, b = 2;
+
+if (a ** 1 == a && b ** 1 == b) {
+  console.log (a - b)
+}
+else {
+  console.log (' a e b precisam ser numeros válidos')
+};
