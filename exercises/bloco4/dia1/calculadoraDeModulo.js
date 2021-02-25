9a323a15a4c33b4c0a124c47b5666e4bd24c626b
Vinicios Fraga <soicinivt@gmail.com>
2021-02-25 11:49:05 -0300

    Calculadora de modulo com switch pronta.

---
+++ exercises/bloco4/dia1/calculadoraDeModulo.js
@@ -0,0 +1,13 @@
+let a = -2, b = 2;
+
+switch (true) {
+  
+  case (a ** 1 !== a || b ** 1 !== b) :
+      console.log ('Os operadores precisam ser numeros válidos!');
+      break;
+      
+  default:
+      console.log(((a)**2)**(1/2));
+      console.log(((b)**2)**(1/2));
+};
+
