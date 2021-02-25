fdfbe6ccc80afc61e8955dcf17c1f2d7883a596b
Vinicios Fraga <soicinivt@gmail.com>
2021-02-25 15:45:17 -0300

    primeiro e segundo requisito feitos.

---
+++ exercises/bloco4/dia2/preExercicios123.js
@@ -0,0 +1,13 @@
+//Obtenha o valor 'Serviços' do array menu:
+
+let menu = ['Home','Serviços','Portigólio','Links'], menuServices = menu[1];
+
+console.log (menuServices);
+
+//Procure o índice do valor 'Portifólio' do array menu:
+
+menu = ['Home', 'Serviços', 'Portifólio', 'Links'];
+let indexOfPortifolio = menu.indexOf('Portifólio');
+
+console.log(indexOfPortifolio);
+
