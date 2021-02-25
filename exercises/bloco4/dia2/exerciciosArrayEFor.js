086d0ceb0e09b275d94414aa2c2ef019e6ea08ae
Vinicios Fraga <soicinivt@gmail.com>
2021-02-25 19:16:30 -0300

    Quarto requisito completo.

--- exercises/bloco4/dia2/exerciciosArrayEFor.js
+++ exercises/bloco4/dia2/exerciciosArrayEFor.js
@@ -24,3 +24,18 @@ for (let numerosPercorridos = 0; numerosPercorridos < numbers.length; numerosPer
     console.log (somaDosNumeros/numbers.length);
   }
 };
+
+//Requisito 4:
+
+for (let numerosPercorridos = 0; numerosPercorridos < numbers.length; numerosPercorridos +=1) {
+    somaDosNumeros += numbers[numerosPercorridos];
+    if (numerosPercorridos === numbers.length -1) {
+      let media = (somaDosNumeros/numbers.length);
+         if (media > 20) {
+           console.log ('Valor maior que 20.');
+         }
+          else {
+            console.log ('Valor menor ou igual a 20.')
+          }
+    }
+  };
