894692ea95c357817d4063cfca9f730d6e7d8fbb
Vinicios Fraga <soicinivt@gmail.com>
2021-02-25 19:04:20 -0300

    Segundo requisito completo.

--- exercises/bloco4/dia2/exerciciosArrayEFor.js
+++ exercises/bloco4/dia2/exerciciosArrayEFor.js
@@ -5,3 +5,12 @@ let numbers = [5,9,3,19,70,8,100,2,35,27];
 for (let numerosPercorridos = 0; numerosPercorridos < numbers.length; numerosPercorridos +=1) {
   console.log (numbers[numerosPercorridos]);
 };
+
+//requisito 2:
+let somaDosNumeros = 0;
+for (let numerosPercorridos = 0; numerosPercorridos < numbers.length; numerosPercorridos +=1) {
+  somaDosNumeros += numbers[numerosPercorridos];
+  if (numerosPercorridos === numbers.length -1) {
+    console.log (somaDosNumeros);
+  }
+};
