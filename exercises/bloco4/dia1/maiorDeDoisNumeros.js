c33b909a897d519267fc802b9c2b4137d8431a86
Vinicios Fraga <soicinivt@gmail.com>
2021-02-25 15:24:08 -0300

    Concluido programa que apresenta o maior enre dois números.

---
+++ exercises/bloco4/dia1/maiorDeDoisNumeros.js
@@ -0,0 +1,63 @@
+let a = 2, b = (-2);
+
+if (a > b) {
+ console.log (a);
+}
+  else if (b > a)
+      {
+      console.log (b);
+  }
+    else if (a == b) {
+      console.log ('a e b possuem números iguais.')
+    }
+      else {
+          console.log ('As variáveis precisam conter valores numericos.')
+      };
+
+      a = 2, b = 22;
+
+      if (a > b) {
+       console.log (a);
+      }
+        else if (b > a)
+            {
+            console.log (b);
+        }
+          else if (a == b) {
+            console.log ('a e b possuem números iguais.')
+          }
+            else {
+                console.log ('As variáveis precisam conter valores numericos.')
+            };
+
+            a = 2, b = 2;
+
+if (a > b) {
+ console.log (a);
+}
+  else if (b > a)
+      {
+      console.log (b);
+  }
+    else if (a == b) {
+      console.log ('a e b possuem números iguais.')
+    }
+      else {
+          console.log ('As variáveis precisam conter valores numericos.')
+      };
+
+      a = 2, b = 'x';
+
+if (a > b) {
+ console.log (a);
+}
+  else if (b > a)
+      {
+      console.log (b);
+  }
+    else if (a == b) {
+      console.log ('a e b possuem números iguais.')
+    }
+      else {
+          console.log ('As variáveis precisam conter valores numericos.')
+      };
