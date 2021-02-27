457db6a88b511b381ecd9addfe0a2ebc59f86fd9
Vinicios Fraga <soicinivt@gmail.com>
2021-02-26 21:07:40 -0300

    3º exercício concluido, triângulo invertido :D

--- exercises/bloco4/dia3/exerciciosB4D3.js
+++ exercises/bloco4/dia3/exerciciosB4D3.js
@@ -1,6 +1,6 @@
 //Exercício 1 :
 
-let valorN = 5;
+let valorN = 10;
 let quadrado = '';
 
 for (let n = 0; n <= valorN * valorN; n += 1) {
@@ -14,7 +14,26 @@ for (let n = 0; n <= valorN * valorN; n += 1) {
 //Exercício 2 :
 
 let triangulo = '';
+
 for (let n = 0; n <= valorN; n += 1) {
   triangulo +='*';
     console.log (triangulo);
 };
+
+//Exercício 3 :
+
+let asterix = '*' , obelix = ' ';
+let trianguloInvertido = '', nAsteriscos = 1, nEspaços = valorN - 1;
+
+for (let n = valorN; n > 0; n -= 1) {
+  for (indexEspaços = 0; indexEspaços < nEspaços; indexEspaços += 1) {
+    trianguloInvertido += obelix;
+  };
+  for (indexAsteriscos = 0; indexAsteriscos < nAsteriscos; indexAsteriscos +=1) {
+    trianguloInvertido += asterix;
+  };
+  console.log (trianguloInvertido);
+  nAsteriscos += 1;
+  nEspaços -= 1;
+  trianguloInvertido = ''
+}
