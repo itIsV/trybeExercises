519ee3714bb46c32520ccc8d2e25e81a5e6bde28
Vinicios Fraga <soicinivt@gmail.com>
2021-03-01 19:22:08 -0300

    Concluido os 5 primeiros exercícios.

--- exercises/bloco4/dia4/preExercicios.js
+++ exercises/bloco4/dia4/preExercicios.js
@@ -42,3 +42,92 @@ let car = {
     console.log(x, fruits[x]);
   };
   
+
+// Exercício 1:
+
+function som (firistNumber, secondNumber) {
+  return firistNumber + secondNumber;
+};
+
+console.log (som (0,2));
+
+
+function subtraction (firistNumber, secondNumber) {
+    return firistNumber - secondNumber;
+  };
+  
+  console.log (subtraction (5,3));
+
+  function multiplication (firistNumber, secondNumber) {
+    return firistNumber * secondNumber;
+  };
+  
+  console.log (multiplication (2,11));
+
+function module (theNumber) {
+ return (theNumber ** 2) ** (1/2)
+};
+
+console.log (module (-22));
+
+// Exercício 2:
+
+function whatIsTheHigherNumber (numberOne, numberTwo) {
+  if (numberOne > numberTwo) {
+    return numberOne;
+  }
+    else {
+      return numberTwo;
+    };
+};
+
+console.log (whatIsTheHigherNumber(2, 22));
+
+
+// Exercício 3:
+
+function whatIsTheHigherNumber (numberOne, numberTwo, numberThree) {
+    if (numberOne > numberTwo && numberOne > numberThree) {
+      return numberOne;
+    }
+      else if (numberTwo > numberOne && numberTwo > numberThree) {
+        return numberTwo;
+      }
+        else {
+          return numberThree;
+        }
+  };
+  
+  console.log (whatIsTheHigherNumber(0, 22, 02));
+
+  // Exercício 4:
+
+  function positiveNegativeOrZero (theNumber) {
+    if (theNumber > 0) {
+      return 'Positive';
+    }
+      else if (theNumber === 0) {
+        return 'ZERO';
+      }
+        else {
+          return 'Negative';
+        };
+  };
+
+  console.log(positiveNegativeOrZero(2));
+
+  // Exercício 5:
+  
+  function isTheTriangleAngle(angle1, angle2, angle3) {
+    if (angle1 + angle2 + angle3 === 180) {
+      return true;
+    }
+      else if (angle1 === 0 || angle2 === 0 || angle3 === 0) {
+        return 'Erro!!!\nÂngulo inválido!';
+      }
+        else {
+          return false;
+        };
+  };
+
+  console.log (isTheTriangleAngle(2, 22, 156));
