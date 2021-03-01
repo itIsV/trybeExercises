53a8c771725410c13f87f346024f642b1440171b
Vinicios Fraga <soicinivt@gmail.com>
2021-03-01 20:13:59 -0300

    Exercício 6 concluido.

--- exercises/bloco4/dia4/preExercicios.js
+++ exercises/bloco4/dia4/preExercicios.js
@@ -117,7 +117,7 @@ function whatIsTheHigherNumber (numberOne, numberTwo, numberThree) {
   console.log(positiveNegativeOrZero(2));
 
   // Exercício 5:
-  
+
   function isTheTriangleAngle(angle1, angle2, angle3) {
     if (angle1 + angle2 + angle3 === 180) {
       return true;
@@ -131,3 +131,41 @@ function whatIsTheHigherNumber (numberOne, numberTwo, numberThree) {
   };
 
   console.log (isTheTriangleAngle(2, 22, 156));
+
+  //Exercício 6:
+
+  function chessMove (piece) {
+    let move = '';
+    switch (true) {
+      
+      case  piece.toLowerCase() === 'rei' :
+        move = 'Pode mover-se em qualquer direção, porém apenas uma casa por vez.';
+        break;
+          
+        case  piece.toLowerCase() === 'dama' :
+            move = 'Assim como o Rei, pode mover-se em qualquer direção (vertical, horizontal e diagonal), porém quantas casas quiser, desde que estejam livres.';
+            break;
+              
+            case  piece.toLowerCase() === 'torre' :
+                move = 'Move-se em linha reta, tanto na vertical quanto na horizontal, quantas casas quiser.';
+                break;
+
+                case  piece.toLowerCase() === 'bispo' :
+                    move = 'Move-se na diagonal, quantas casas quiser. O Bispo que iniciar a partida em uma casa branca somente poderá transitar pelas brancas, enquanto o Bispo que inicia em uma casa preta somente poderá transitar pelas casas pretas.';
+                    break;
+
+                    case  piece.toLowerCase() === 'cavalo' :
+                        move = 'É a única peça que pode saltar sobre as outras peças do tabuleiro, sejam elas amigas ou inimigas. O movimento executado pelo Cavalo é conhecido por “um-dois” ou “em L”. Ele pode andar duas casas na horizontal e uma na vertical, ou duas na vertical e uma na horizontal, uma na horizontal e duas na vertical, e assim por diante. Quando a casa de saída do Cavalo for escura, a casa de chegada será clara, e vice-versa.';
+                        break;
+
+                        case  piece.toLowerCase() === 'peão' :
+                            move = 'Move-se sempre uma casa para frente, exceto no primeiro movimento, quando pode mover-se duas casas. O peão é a única peça que não pode retroceder, e também a única que efetua a captura com um movimento diferente do utilizado para avançar no tabuleiro. O peão pode capturar as peças que estejam uma fileira acima, mas nas colunas adjacentes a sua.';
+                            break;
+
+    }
+    return move;
+  };
+
+  console.log (chessMove('Cavalo'));
+
+  
