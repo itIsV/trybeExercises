56ee1b41c9b459e61908bb94f8bc51051ecb22b2
Vinicios Fraga <soicinivt@gmail.com>
2021-03-01 16:12:15 -0300

    Pré exercício 2.

--- exercises/bloco4/dia4/preExercicios.js
+++ exercises/bloco4/dia4/preExercicios.js
@@ -19,4 +19,26 @@ console.log ('A jogadora ' + individuo.name
  console.log ('A jogadora possui ' + individuo.medals.golden +
  ' medalhas de ouro e ' + individuo.medals.silver + ' medalhas de prata.' );
 
+// Pré exercício 2:
 
+let cars = ["Saab", "Volvo", "BMW"];
+
+for (let v in cars) {
+  console.log(cars[v]);
+}
+
+let car = {
+    type: "Fiat",
+    model: "500",
+    color: "white",
+  };
+  
+  for (let v in car) {
+    console.log(v, car[v]);
+  };
+
+  let fruits = ["Apple", "Banana", "Orange"];
+  for (let x in fruits) {
+    console.log(x, fruits[x]);
+  };
+  
