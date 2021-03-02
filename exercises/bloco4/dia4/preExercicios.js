cabc7d3088eb63e653c245700ec0494b34bb33fb
Vinicios Fraga <soicinivt@gmail.com>
2021-03-01 22:35:42 -0300

    Último exercício de refatoramento concluido.

--- exercises/bloco4/dia4/preExercicios.js
+++ exercises/bloco4/dia4/preExercicios.js
@@ -215,7 +215,7 @@ function seraQueTemUmPar (numberOne, two, three) {
 
 console.log (seraQueTemUmPar(0,1,2));
 
-//Exercício 9:
+// Exercício 9:
 
 function seraQueTemUmImpar (numberOne, two, three) {
   return numberOne % 2 != 0 || two % 2 != 0 || three % 2 != 0;
@@ -223,10 +223,69 @@ function seraQueTemUmImpar (numberOne, two, three) {
 
 console.log (seraQueTemUmImpar(0,2,22));
 
+// Exercício 10:
+
+function lucro (valorCusto, valoVenda){
+let valorTotalDeCusto =  ((20/100) * valorCusto) + valorCusto;
+if (valoVenda < 0 || valorCusto < 0) {
+  return 'erro';
+}
+ else {
+     let lucroReal = (valoVenda - valorTotalDeCusto) * 1000;
+     return lucroReal;
+ };
+};
+
+console.log(lucro(5, 20));
+
+//Exercício 11:
+
+function salario(salarioBruto) {
+let inss = 0;
+
+switch (true) {
+  
+  case salarioBruto <= 1556.94 :
+  inss += (8/100) * salarioBruto;
+    break;
+      case salarioBruto <= 2594.92 :
+      inss += (9/100) * salarioBruto;
+        break;
+          case salarioBruto <= 5189.82 :
+          inss += (11/100) * salarioBruto;
+            break;
+              case salarioBruto > 5189.82 :
+              inss += 570.88;
+                break;
+}
 
 
+let salarioBase = salarioBruto - inss;
 
 
+let ir = 0;
 
+switch (true) {
+  
+    case salarioBase <= 1903.98 :
+    console.log (salarioBase);
+      break;
+        case salarioBase <= 2826.65 :
+        ir += (7.5/100) * salarioBase - 142.80;
+          break;
+            case salarioBase <= 3751.05 :
+            ir += (15/100) * salarioBase - 354.80;
+              break;
+                case salarioBase <= 4664.68 :
+                ir += (22.5/100) * salarioBase - 636.13;
+                  break;
+                  case salarioBase > 4664.68 :
+                    ir += (27.5/100) * salarioBase - 869.36;
+                      break;
+  };
 
+  let salarioLiquido = salarioBase - ir;
+  return salarioLiquido;
+}
 
+  console.log (salario(3500));
