20d40a1cc4f20b40bb409452fb7c5f57306d03ef
Vinicios Fraga <soicinivt@gmail.com>
2021-03-16 01:17:51 -0300

    Quebrando a cabeça no exercicio part 1.

--- exercises/bloco6/dia1/script.js
+++ exercises/bloco6/dia1/script.js
@@ -12,3 +12,55 @@ function setOptions(states, value) {
 }
 
 setOptions(states, value);
+
+/* usei como referencia :https://www.youtube.com/watch?v=e57ReoUn6kM */
+const data = document.querySelector('#start');
+data.addEventListener('input', () => {
+  let theVerificador = data.value.length;
+  let counter = 0;
+  
+  if (theVerificador === 2 ) {
+    data.value += '/';
+  }
+  
+  if (theVerificador === 5 ) {
+    data.value += '/';
+  } 
+  
+  if (data.value.includes('//')) {     
+    data.value = '';
+  }
+  
+  let dataValue = data.value;
+  let day = dataValue[0] + dataValue[1];
+  let month = dataValue[3] + dataValue[4];
+  let year = dataValue[6];
+  
+  if (day < 1 || day > 31 || month < 1 || month > 12 || year < 1) {
+    data.value = '';  
+    alert('data inválida');
+  }
+})
+
+function xab() {
+    const divData = document.createElement('div');
+    const name = document.querySelector('#theName').value;
+    const email = document.querySelector('#theEmail').value;
+    const cpf = document.querySelector('#theCPF').value;
+    const address = document.querySelector('#theAddress').value;
+    const city = document.querySelector('#theCity').value;
+    const state = document.querySelector('#theState').value;
+    //const radio = document.querySelector('#radio').innerHTML.value;
+    const curriculum = document.querySelector('#curriculum').value;
+    const position = document.querySelector('#position').value;
+    const roleDescription = document.querySelector('#roleDescription').value;
+    const start = document.querySelector('#start').value;
+    divData.classList.add('data');
+    divData.innerHTML = name, email;
+    return divData.value;
+}
+
+const theButton = document.querySelector('#submitButton');
+theButton.addEventListener('click', xab())
+
+let vai = xab();
