40e7f3c5cfaee6105e830373175cfa5884a87b59
Vinicios <soicinivt@gmail.com>
2021-04-18 17:12:27 -0300

    mais um exercicio que foi proposto no dia.

--- exercises/bloco8/dia1/scripts.js
+++ exercises/bloco8/dia1/scripts.js
@@ -1,13 +1,19 @@
-const employedPerson = (nomeCompleto) => ({nomeCompleto, email:`${nomeCompleto.split(' ')[0]}_${nomeCompleto
-  .split(' ')[1]}@trybe.com`});
+// const employedPerson = (nomeCompleto) => ({nomeCompleto, email:`${nomeCompleto.split(' ')[0]}_${nomeCompleto
+//   .split(' ')[1]}@trybe.com`});
 
-const newEmployees = () => {
-  const employees = {
-      id1: employedPerson('Pedro Guerra'),
-      id2: employedPerson('Luiza Drumond'),
-      id3: employedPerson('Carla Paiva')
-    };
-  return employees;
-};
+// const newEmployees = () => {
+//   const employees = {
+//       id1: employedPerson('Pedro Guerra'),
+//       id2: employedPerson('Luiza Drumond'),
+//       id3: employedPerson('Carla Paiva')
+//     };
+//   return employees;
+// };
 
-console.log(newEmployees());
+// console.log(newEmployees());
+
+const check = (number, action) => number === action() ? console.log('Parabéns você ganhou') : console.log('Tente novamente');
+
+const between1And5 = () => Math.ceil(Math.random() * 5);
+
+check(2, between1And5);
