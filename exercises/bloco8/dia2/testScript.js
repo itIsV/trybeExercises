bc6a8940d6572cb6d3352244843add1b5a1afd96
Vinicios <soicinivt@gmail.com>
2021-04-18 20:49:14 -0300

    Testes com forEach do dia.

--- exercises/bloco8/dia2/testScript.js
+++ exercises/bloco8/dia2/testScript.js
@@ -1,16 +1,33 @@
-const students = [
-  { name: 'Maria', grade: 70, approved: '' },
-  { name: 'José', grade: 56, approved: '' },
-  { name: 'Roberto', grade: 90, approved: '' },
-  { name: 'Ana', grade: 81, approved: '' },
+// const students = [
+//   { name: 'Maria', grade: 70, approved: '' },
+//   { name: 'José', grade: 56, approved: '' },
+//   { name: 'Roberto', grade: 90, approved: '' },
+//   { name: 'Ana', grade: 81, approved: '' },
+// ];
+
+// const isApproved = (array) => array.forEach((({grade}, index) => {
+//     grade >= 60 ? array[index].approved = 'Approved' : array[index].approved = 'Reproved';
+//     }
+//   )
+// );
+
+// isApproved(students);
+
+// console.log(students);
+
+// const firstMultipleOfFive = (array) => array.find(number => number % 5 === 0);
+
+// console.log(firstMultipleOfFive([11, 24, 39, 47, 50, 62, 75, 81, 96, 100]));
+
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
 ];
 
-const isApproved = (array) => array.forEach((({grade}, index) => {
-    grade >= 60 ? array[index].approved = 'Approved' : array[index].approved = 'Reproved';
-    }
-  )
-);
+const showEmailList = (email, index) => {
+  console.log(`${index + 1}º: O email ${email} esta cadastrado em nosso banco de dados!`);
+};
 
-isApproved(students);
-
-console.log(students);
+emailListInData.forEach(showEmailList);
