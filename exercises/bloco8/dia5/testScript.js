6f43f64753b14639e6d52545301b8bb793d4417b
Vinicios <soicinivt@gmail.com>
2021-04-20 10:14:37 -0300

    Teste que subistitui valores com array destructuring.

--- exercises/bloco8/dia5/testScript.js
+++ exercises/bloco8/dia5/testScript.js
@@ -20,22 +20,28 @@ const product = {
 // const printObjectValues2Ponto0 = ({name, seller, price}) => ` Super promoção!!!\n${name} na ${seller} por apenas: ${price}!!`
 // console.log(printObjectValues2Ponto0(product));
 
-const student = {
-  a: 'Maria',
-  b: 'Turma B',
-  c: 'Matematica',
-};
+// const student = {
+//   a: 'Maria',
+//   b: 'Turma B',
+//   c: 'Matematica',
+// };
 
-const { a: name, b: classAssigned, c: subject } = student;
+// const { a: name, b: classAssigned, c: subject } = student;
 
-console.log(name); // Maria
-console.log(classAssigned); // Turma B
-console.log(subject); // Matemática
+// console.log(name); // Maria
+// console.log(classAssigned); // Turma B
+// console.log(subject); // Matemática
 
-const arrayCountries = ['Brazil', 'Japan', 'China', 'Canada'];
-const [firstCountry, secondCountry, thirdCountry, fourthCountry] = arrayCountries;
+// const arrayCountries = ['Brazil', 'Japan', 'China', 'Canada'];
+// const [firstCountry, secondCountry, thirdCountry, fourthCountry] = arrayCountries;
 
-console.log(firstCountry); // Brazil
-console.log(secondCountry); // Japan
-console.log(thirdCountry); // China
-console.log(fourthCountry); // Canada
+// console.log(firstCountry); // Brazil
+// console.log(secondCountry); // Japan
+// console.log(thirdCountry); // China
+// console.log(fourthCountry); // Canada
+
+let a = 8, b = 6;
+console.log(`agora a vale: ${a} e b vale: ${b}`);
+                  [a, b] = [b, a];
+console.log('---------------------------');
+console.log(`agora a vale: ${a} e b vale: ${b}`);
