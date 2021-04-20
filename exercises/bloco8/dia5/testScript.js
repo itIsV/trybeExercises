a01817d85261263c7a87b37e99ec7395293f4a7f
Vinicios <soicinivt@gmail.com>
2021-04-20 09:57:05 -0300

    Exercicios test.

--- exercises/bloco8/dia5/testScript.js
+++ exercises/bloco8/dia5/testScript.js
@@ -1,9 +1,33 @@
 // hello code
 
-const specialFruit = ['Banana', 'Manga', 'maça'];
+// const specialFruit = ['Banana', 'Manga', 'maça'];
 
-const additionalItens = ['Mel', 'Cereja picada'];
+// const additionalItens = ['Mel', 'Cereja picada'];
 
-const fruitSalad = (specialFruit, additionalItens) => [...specialFruit, ...additionalItens];
+// const fruitSalad = (specialFruit, additionalItens) => [...specialFruit, ...additionalItens];
 
-console.log(fruitSalad(specialFruit, additionalItens));
+// console.log(fruitSalad(specialFruit, additionalItens));
+
+const product = {
+  name: 'Smart TV Crystal UHD',
+  price: '1899.05',
+  seller: 'Casas de Minas',
+};
+
+// const printObjectValues = (object) => `${Object.values(object)}`;
+// console.log(printObjectValues(product));
+
+// const printObjectValues2Ponto0 = ({name, seller, price}) => ` Super promoção!!!\n${name} na ${seller} por apenas: ${price}!!`
+// console.log(printObjectValues2Ponto0(product));
+
+const student = {
+  a: 'Maria',
+  b: 'Turma B',
+  c: 'Matematica',
+};
+
+const { a: name, b: classAssigned, c: subject } = student;
+
+console.log(name); // Maria
+console.log(classAssigned); // Turma B
+console.log(subject); // Matemática
