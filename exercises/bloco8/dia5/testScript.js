4c8de225034f4139712f6c37285b6cbf40d5f3fd
Vinicios <soicinivt@gmail.com>
2021-04-19 23:50:19 -0300

    Alguns testes.

--- exercises/bloco8/dia5/testScript.js
+++ exercises/bloco8/dia5/testScript.js
@@ -1,2 +1,16 @@
 // hello code
 
+const pessoas = [
+  {
+    peso:60,
+    altura:2.0,
+    codeName:'Palito',
+    }
+]
+
+const imc = ({peso}, {altura}, {nome}) => {
+  const imc = (peso / (altura * altura)).toFixed(2);
+  return console.log(`O IMC de ${nome} é: ${imc}`);
+}
+
+console.log(Object.entries(...pessoas));
