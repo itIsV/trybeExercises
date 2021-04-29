a8b59a2c18f3a37313103441b7ad519be9bf8606
Vinicios <soicinivt@gmail.com>
2021-04-29 11:36:07 -0300

    Testes e mais testes.

--- exercises/bloco10/dia2/testScripts.test.js
+++ exercises/bloco10/dia2/testScripts.test.js
@@ -55,3 +55,49 @@ describe('Quando o tipo do animal, não existe', () => {
     );
   });
 });
+
+test('Testando com async/await', async () => {
+  const listDogs = await getListAnimals('Dog');
+  expect(listDogs[0].name).toEqual('Dorminhoco');
+  expect(listDogs[1].name).toEqual('Soneca');
+});
+
+describe('Testando promise - findAnimalsByType', () => {
+  describe('Quando o tipo do animal existe', () => {
+    test('Retorne a lista de animais', () => {
+      const listDogs = [
+        { name: 'Dorminhoco', age: 1, type: 'Dog' },
+        { name: 'Soneca', age: 2, type: 'Dog' },
+      ]
+      expect.assertions(1);
+      return expect(getListAnimals('Dog')).resolves.toEqual(listDogs)
+    });
+  });
+
+  describe('Quando o tipo de animal não existe', () => {
+    test('Retorna um erro', () => {
+      expect.assertions(1);
+      return expect(getListAnimals('Lion')).rejects.toEqual({ error: 'Não possui esse tipo de animal.' })
+    });
+  });
+});
+
+describe('Testando Async/Await - findAnimalsByType', () => {
+  describe('Quando o tipo do animal existe', () => {
+    test('Retorne a lista de animais', async () => {
+      const listDogs = [
+        { name: 'Dorminhoco', age: 1, type: 'Dog' },
+        { name: 'Soneca', age: 2, type: 'Dog' },
+      ]
+      expect.assertions(1);
+      await expect(getListAnimals('Dog')).resolves.toEqual(listDogs)
+    });
+  });
+
+  describe('Quando o tipo de animal não existe', () => {
+    test('Retorna um erro', async () => {
+      expect.assertions(1);
+      await expect(getListAnimals('Lion')).rejects.toEqual({ error: 'Não possui esse tipo de animal.' })
+    });
+  });
+});
